c8967f232b78d833f5079f341c001c7bc41fe53c
Carlos Waldo Rojas <krlosw9@gmail.com>
2024-12-12 10:55:07 -0500

    Utilizando fuentes de google fonts optimizadas por next

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,11 +1,17 @@
+import { montserrat } from './ui/fonts';
+import './ui/global.css'
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="es">
+      <body className={`${montserrat.className} antialiased`}>
+        <h1>Este titulo</h1>
+        {children}
+      </body>
     </html>
   );
 }
